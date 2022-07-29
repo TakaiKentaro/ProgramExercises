c50a38f459f0893c154d3347c6fb28c000f77cf6
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-07-29 11:59:39 +0900

    [add]Reversi制作開始

---
+++ Assets/課題 2022前期/Reversi/ReversiCellScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public enum ReversiCellState
+{
+    Empty = 0,
+    Black = 1,
+    White = 2,
+}
+
+/// <summary>
+/// クラス説明
+/// </summary>
+public class ReversiCellScript : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/課題 2022前期/Reversi/ReversiScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+/// <summary>
+/// クラス説明
+/// </summary>
+public class ReversiScript : MonoBehaviour
+{
+    [Tooltip("行数"), SerializeField] int _rows = 0;
+    [Tooltip("列数"), SerializeField] int _colums = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
