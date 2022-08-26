be84ee56b4a5dbd5025a2ce39a51725ffadc42ee
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-08-26 13:39:28 +0900

    [update]改良

---
+++ Assets/課題 2022前期/Reversi/ReversiBord.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ReversiBordState
+{
+    Empty = 0,
+    Select = 1,
+    Placed = 2,
+}
+
+/// <summary>
+/// クラス説明
+/// </summary>
+public class ReversiBord : MonoBehaviour
+{
+    [Header("LifeGameCellCellState番号"), Tooltip("LifeGameCellCellState番号"), SerializeField] ReversiBordState _reversiBordState = ReversiBordState.Empty;
+
+    [Tooltip("Cell")] public ReversiCellScript _reversiCell;
+
+    [SerializeField, Tooltip("Color")] Image _image;
+
+    [Tooltip("Reversi")] ReversiScript _reversi;
+
+    public ReversiBordState CellState
+    {
+        get => _reversiBordState;
+        set
+        {
+            _reversiBordState = value;
+            StateChanged();
+        }
+    }
+    void Start()
+    {
+        _image = gameObject.GetComponent<Image>();
+
+        _reversi = transform.parent.GetComponent<ReversiScript>();
+    }
+    private void OnValidate()
+    {
+        StateChanged();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void StateChanged()
+    {
+        Debug.Log("StateAnim");
+        if (_reversiBordState == ReversiBordState.Empty)
+        {
+            _image.color = Color.green;
+        }
+        else if (_reversiBordState == ReversiBordState.Select)
+        {
+            _image.color = Color.red;
+        }
+        else if (_reversiBordState == ReversiBordState.Placed)
+        {
+            _image.color = Color.yellow;
+        }
+    }
+}
--- Assets/課題 2022前期/Reversi/ReversiScript.cs
+++ Assets/課題 2022前期/Reversi/ReversiScript.cs
@@ -10,14 +10,18 @@ public class ReversiScript : MonoBehaviour
 {
     [Tooltip("行数")] int _rows = 8;
     [Tooltip("列数")] int _colums = 8;
+    [Tooltip("赤の場所Y")] int _YredPoint = 0;
+    [Tooltip("赤の場所X")] int _XredPoint = 0;
 
-    [SerializeField,Tooltip("Bord")] GameObject _bordCell = null;
-    [Tooltip("BordCellの配列")] GameObject[,] _bordCells = null;
+    [SerializeField, Tooltip("Bord")] ReversiBord _bordCell = null;
+    [Tooltip("BordCellの配列")] public ReversiBord[,] _bordCells = null;
 
     [Tooltip("GridLayoutGroup"), SerializeField] GridLayoutGroup _gridLayoutGroup = null;
-    void Start()
+
+    [Header("譜面入力"), SerializeField, Tooltip("譜面入力")] string _bordSheet;
+    void Awake()
     {
-        _bordCells = new GameObject[_rows, _colums];
+        _bordCells = new ReversiBord[_rows, _colums];
 
         gameObject.GetComponent<GridLayoutGroup>().constraintCount = _colums;
 
@@ -30,7 +34,71 @@ public class ReversiScript : MonoBehaviour
             for (var c = 0; c < _colums; c++)
             {
                 var bord = Instantiate(_bordCell, _gridLayoutGroup.transform);
-                _bordCells[r, c] = bord;    
+                _bordCells[r, c] = bord;
+                if (r == 0 && c == 0)
+                {
+                    bord.CellState = ReversiBordState.Select;
+                }
+                else if (r == 3 && c == 3 || r == 4 && c == 4)
+                {
+                    bord._reversiCell.CellState = ReversiCellState.White;
+                }
+                else if (r == 4 && c == 3 || r == 3 && c == 4)
+                {
+                    bord._reversiCell.CellState = ReversiCellState.Black;
+                }
+                else
+                {
+                    bord.CellState = ReversiBordState.Empty;
+                }
+
+            }
+        }
+    }
+    private void Update()
+    {
+        SelectBord();
+    }
+
+    void SelectBord()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) // 左キーを押した
+        {
+            if (_XredPoint > 0)
+            {
+                _XredPoint--;
+                _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Select;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D)) // 右キーを押した
+        {
+            if (_XredPoint < _rows - 1)
+            {
+                _XredPoint++;
+                _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Select;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W)) // 上キーを押した
+        {
+            if (_YredPoint > 0)
+            {
+                _YredPoint--;
+                _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Select;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S)) // 下キーを押した
+        {
+            if (_YredPoint < _colums - 1)
+            {
+                _YredPoint++;
+                _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Select;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(_bordCells[_YredPoint, _XredPoint].CellState == ReversiBordState.Placed)
+            {
+                _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState = ReversiCellState.White;
             }
         }
     }
