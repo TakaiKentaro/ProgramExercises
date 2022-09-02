5163263729cff4ace93c870518fc243fbeea5063
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-09-02 12:08:30 +0900

    [update]ReversiScriptかきかえ　

--- Assets/課題 2022前期/Reversi/ReversiScript.cs
+++ Assets/課題 2022前期/Reversi/ReversiScript.cs
@@ -19,6 +19,8 @@ public class ReversiScript : MonoBehaviour
     [Tooltip("GridLayoutGroup"), SerializeField] GridLayoutGroup _gridLayoutGroup = null;
 
     [Header("譜面入力"), SerializeField, Tooltip("譜面入力")] string _bordSheet;
+
+    [Tooltip("falseが黒・trueが白を置ける")] public bool _turnCheck = false;
     void Awake()
     {
         _bordCells = new ReversiBord[_rows, _colums];
@@ -26,6 +28,10 @@ public class ReversiScript : MonoBehaviour
         gameObject.GetComponent<GridLayoutGroup>().constraintCount = _colums;
 
         CreatGrid();
+    }
+    private void Start()
+    {
+        _turnCheck = false;
     }
     void CreatGrid()
     {
@@ -66,6 +72,7 @@ public class ReversiScript : MonoBehaviour
         {
             if (_XredPoint > 0)
             {
+                _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Empty;
                 _XredPoint--;
                 _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Select;
             }
@@ -74,6 +81,7 @@ public class ReversiScript : MonoBehaviour
         {
             if (_XredPoint < _rows - 1)
             {
+                _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Empty;
                 _XredPoint++;
                 _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Select;
             }
@@ -82,6 +90,7 @@ public class ReversiScript : MonoBehaviour
         {
             if (_YredPoint > 0)
             {
+                _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Empty;
                 _YredPoint--;
                 _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Select;
             }
@@ -90,16 +99,49 @@ public class ReversiScript : MonoBehaviour
         {
             if (_YredPoint < _colums - 1)
             {
+                _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Empty;
                 _YredPoint++;
                 _bordCells[_YredPoint, _XredPoint].CellState = ReversiBordState.Select;
             }
         }
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)) // Cellを配置
         {
-            if(_bordCells[_YredPoint, _XredPoint].CellState == ReversiBordState.Select)
+            if (_bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.Empty)
             {
-                _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState = ReversiCellState.White;
+                if (!_turnCheck) _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState = ReversiCellState.Black;
+                else _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState = ReversiCellState.White;
+                BordCheck(_XredPoint, _YredPoint);
             }
+        }
+    }
+
+    /// <summary>
+    /// 盤面を処理
+    /// </summary>
+    /// <returns></returns>
+    void BordCheck(int x, int y)
+    {
+        if(!_turnCheck) // 黒の盤面生成
+        {
+            if (x + 1 < _rows && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.White) {  } //右
+            if (x - 1 >= 0 && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.White) {  } //左
+            if (y + 1 < _colums && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.White) {  } //下
+            if (y - 1 >= 0 && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.White) {  } //上
+            if (x + 1 < _rows && y + 1 < _colums && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.White) {  } //右下
+            if (x - 1 >= 0 && y - 1 >= 0 && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.White) {  }//左上
+            if (x + 1 < _rows && y - 1 >= 0 && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.White) {  } //左下
+            if (x - 1 >= 0 && y + 1 < _colums && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.White) {  } //右上
+        }
+        else // 白の盤面生成
+        {
+            if (x + 1 < _rows && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.Black) { } //右
+            if (x - 1 >= 0 && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.Black) { } //左
+            if (y + 1 < _colums && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.Black) { } //下
+            if (y - 1 >= 0 && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.Black) { } //上
+            if (x + 1 < _rows && y + 1 < _colums && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.Black) { } //右下
+            if (x - 1 >= 0 && y - 1 >= 0 && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.Black) { }//左上
+            if (x + 1 < _rows && y - 1 >= 0 && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.Black) { } //左下
+            if (x - 1 >= 0 && y + 1 < _colums && _bordCells[_YredPoint, _XredPoint]._reversiCell.CellState == ReversiCellState.Black) { } //右上
         }
     }
 }
