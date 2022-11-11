6c5fa7e67e80bc4317c08b1a42ec958a3b142390
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-11-11 13:33:16 +0900

    [add]Week4の内容を作製

---
+++ Assets/授業/後期/Week4/Week4Sample.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Week4Sample : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+}
