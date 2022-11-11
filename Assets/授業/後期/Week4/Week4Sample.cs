5c3ceb971101bb1cee0f8814a9161029b11b1754
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-11-11 15:55:21 +0900

    [update]更新

--- Assets/授業/後期/Week4/Week4Sample.cs
+++ Assets/授業/後期/Week4/Week4Sample.cs
@@ -8,6 +8,16 @@ public class Week4Sample : MonoBehaviour
 {
     void Start()
     {
+        RotateAsync().MoveNext();
+    }
 
+    private IEnumerator RotateAsync()
+    {
+        Debug.Log("RotateAsync");
+        while (RotateAsync().MoveNext())
+        {
+            transform.Rotate(0, 1, 0);
+            yield return null;
+        }
     }
 }
