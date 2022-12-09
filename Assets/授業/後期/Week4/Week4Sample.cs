72429a8a308bcfcb9dfeb6a4007b80650623fc58
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-12-09 14:21:58 +0900

    [update]20221209の内容を追記

--- Assets/授業/後期/Week4/Week4Sample.cs
+++ Assets/授業/後期/Week4/Week4Sample.cs
@@ -1,23 +1,78 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using UnityEngine;
 
 public class Week4Sample : MonoBehaviour
 {
-    private void Update()
+    private void Start()
     {
-        RotateAsync().MoveNext();
+        StartCoroutine(ExecuteAsync());
     }
 
-    private IEnumerator RotateAsync()
+    private IEnumerator ExecuteAsync()
     {
-        Debug.Log("RotateAsync");
+        Debug.Log("ExecuteAsync: Begin");
+
         while (true)
         {
-            transform.Rotate(0, 1, 0);
+            var t = 0F;
+
+            // X軸の回転を2秒間
+            yield return RotateAsync(Vector3.right, 2);
+
+            //5秒待つかクリックされるのを待つ
+            yield return WhenAny(new WaitForSeconds(5), StartCoroutine(WaitMouseButton(0)));
+
+            // Y軸の回転を2秒間
+            yield return RotateAsync(Vector3.up, 2);
+
+            // 1秒間待機する
+            yield return new WaitForSeconds(1);
+
+            // Z軸の回転を2秒間
+            yield return RotateAsync(Vector3.forward, 2);
+
+            // 1秒間待機する
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    private IEnumerator WaitClick(float timeout)
+    {
+        var c = StartCoroutine(WaitMouseButton(0));
+        yield return WhenAny(new WaitForSeconds(timeout), c);
+    }
+
+    /// <summary>
+    /// マウスの入力を待つコルーチン
+    /// </summary>
+    /// <param name="button"></param>
+    /// <returns></returns>
+    private IEnumerator WaitMouseButton(int button)
+    {
+        while (!Input.GetMouseButtonDown(button)) { yield return null; }
+    }
+
+    /// <summary>
+    /// 2つのコルーチンを結合（どちらかが終わったら終了するコルーチン）
+    /// </summary>
+    private IEnumerator WhenAny(YieldInstruction c1, YieldInstruction c2)
+    {
+        bool result = false;
+        // c1 が終了したら result を true にする別の非同期処理を発火
+        // c2 が終了したら result を true にする別の非同期処理を発火
+        while (!result) { yield return null; }
+    }
+
+    private IEnumerator RotateAsync(Vector3 eulers, float duration)
+    {
+        Debug.Log($"RotateAsync: Begin eulers={eulers}, duration={duration}");
+        var t = 0F;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            transform.Rotate(eulers);
             yield return null;
         }
+        Debug.Log("ExecuteAsync: End");
     }
 }
