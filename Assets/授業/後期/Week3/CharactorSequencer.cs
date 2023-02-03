c97b7bdc8ea486b13836c66ee1ecdd0c3264679e
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-02-03 13:03:31 +0900

    [add]CharactorSequencer.cs作成

---
+++ Assets/授業/後期/Week3/CharactorSequencer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharactorSequencer : MonoBehaviour
+{
+    [SerializeField] private Image _imagePositionLeft; // 左画像
+    [SerializeField] private Image _imagePositionSenter; // 中央画像
+    [SerializeField] private Image _imagePositionRight; // 右画像
+
+    [SerializeField] private Sprite[] _charactorSprite;
+    
+}
